06d0c71257d6eda5668d93b4054d5e8435eb686f
KelvinLopes <kelvin7android@gmail.com>
2020-10-14 00:17:04 +0000

    Add: migrations, routes, sqlite, controllers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import './database/connection';
+import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
+
 app.listen(3333);
+console.log('Running server in http:localhost:3333 🚀');
