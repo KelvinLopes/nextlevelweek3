8634eddd4fe8f2c91fcbac27eb206c8c025cd92c
KelvinLopes <kelvin7android@gmail.com>
2020-10-14 02:35:41 +0000

    Add Modules Images to Orphanage, Multer to config upload

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -31,10 +31,17 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+
         } = request.body;
 
         const orphanagesRepository = getRepository(Orphanage);
+
+        const requestImages = request.files as Express.Multer.File[];
+
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
 
         const orphanage = orphanagesRepository.create({
             name,
@@ -43,7 +50,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
         });
 
         await orphanagesRepository.save(orphanage);
